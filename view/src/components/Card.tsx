bd75885aa7b9f647a26111e243b7085ace720cd4
mosen elsisi <mohsenelsisi36@gmail.com>
2023-03-16 09:31:24 +0200

    card pop animations

--- view/src/components/Card.tsx
+++ view/src/components/Card.tsx
@@ -1,6 +1,12 @@
+import { onMount } from "solid-js";
+
 export default function Card({ title, children: text }: CardProps) {
+  let card: HTMLElement;
+
+  onMount(() => unhidingObserver.observe(card))
+  
   return (
-    <article class="card">
+    <article class="card hide" ref={card}>
       <h3 class="small-heading">{title}</h3>
       <p>{text}</p>
     </article>
@@ -11,3 +17,9 @@ export interface CardProps {
   title: string;
   children?: string;
 }
+
+const unhidingObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) entry.target.classList.remove("hide");
+  });
+});
