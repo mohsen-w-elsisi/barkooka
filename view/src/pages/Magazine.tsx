7acdf54db7c48ae449db5f90a9864a02c1e670d6
mosen elsisi <mohsenelsisi36@gmail.com>
2023-03-16 12:05:31 +0200

    fixed magazine thingy bug

--- view/src/pages/Magazine.tsx
+++ view/src/pages/Magazine.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "@solidjs/router";
 import { createSignal } from "solid-js";
-import renderContentViews from "../components/content_views/renderContentViews";
 import { getMagazineNumber } from "../lib/getMagzine";
+import renderContentViews from "../components/content_views/renderContentViews";
 
 export default function Magazine() {
   const { week } = useParams();
@@ -11,5 +11,5 @@ export default function Magazine() {
     updateContents(renderContentViews(magazine.content))
   );
 
-  return contents();
+  return <>{contents()}</>;
 }
